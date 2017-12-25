4b3e061f27237d9c00528f378d2f19d24351fe44
skycolor <873276172@qq.com>
2017-12-25 16:53:54 +0800

    编辑器demo，基于vue-quill-editor

--- src/components/tool/editor.vue
+++ src/components/tool/editor.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="container" >
-     <quill-editor>
-     	
-     </quill-editor>
+  	 <p class="tit" >
+  	 	Vue-Quill-Editor：基于Quill、适用于Vue2的富文本编辑器。 访问地址：
+  	 	<a href="https://github.com/surmon-china/vue-quill-editor" >vue-quill-editor</a>
+  	 	<el-button class="commitBtn" type="primary" size="small" @click="commit" >
+  	 		提交
+  	 	</el-button>
+  	 </p>
+     <quill-editor
+		class="content"
+		v-model="content"
+      />
   </div>
 </template>
 
@@ -14,6 +22,20 @@ import 'quill/dist/quill.bubble.css'
 import { quillEditor } from 'vue-quill-editor'
 
 export default {
+  data(){
+  	return {
+  		content : ''
+  	}
+  } ,
+  methods : {
+	commit(){
+		this.$message({
+          dangerouslyUseHTMLString: true,
+          message: this.content ,
+          type: 'success'
+        });
+	}
+  } ,
   components: {
     quillEditor
   }
