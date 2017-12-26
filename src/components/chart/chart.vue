e33b3d439e029f52591d3f35057c1c298bcf81ed
skycolor <873276172@qq.com>
2017-12-26 22:21:59 +0800

    图表使用vue-echart ，将安装包以及页面初始化进去了

--- src/components/chart/chart.vue
+++ src/components/chart/chart.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-     <h2>图表页</h2>
+  <div class="container" >
+     <p class="tit" >
+  	 	此处使用echart，这个是非百度官方朋友封装的echart vue工具，基本使用同echart
+  	 	，个人感觉比较习惯，访问地址为：
+  	 	<a href="https://justineo.github.io/vue-echarts/demo/" >vue-echarts</a>
+  	 </p>
   </div>
 </template>
 
@@ -12,5 +16,5 @@ export default {
 
 
 <style lang="scss" scoped >
-	
+	@import '../../style/chart/chart';
 </style>
