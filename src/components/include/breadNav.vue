13b28d1acb829941cb1f64b6798deef4e85b146c
skycolor <873276172@qq.com>
2017-12-27 15:46:51 +0800

    面包屑开头

---
+++ src/components/include/breadNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  	
+  </div>
+</template>
+
+<script>
+	export default {
+	  	 
+	}
+</script>
+
+<style lang="scss" scoped >
+	@import '../../style/include/breadNav';
+</style>
